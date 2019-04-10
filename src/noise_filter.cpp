3a6fe731d53b152f237f96cd5e65819a44e40ebe
Zeno <uwwrv@student.kit.edu>
2019-04-10 20:43:05 +0200

    change to buffer mode filter

--- src/noise_filter.cpp
+++ src/noise_filter.cpp
@@ -1,16 +1,21 @@
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
+#include <queue>
+#include <vector>
+#include <boost/foreach.hpp>
 
 class noiseFilter{
     public:
         noiseFilter():
             nh_()    
         {
-            isFirst=true;
+            isFull=false;
+            count=0;
             nh_.param<std::string>("/name_input",name_input,"/sick_visionary_t_driver/laserscan");
             nh_.param<std::string>("/name_output",name_output,"/sick_visionary_t_driver/laserscan_filtered");
-            laser_input = nh_.subscribe(name_input, 1, &noiseFilter::Callback, this);
-            laser_output = nh_.advertise<sensor_msgs::LaserScan>(name_output, 1);
+            nh_.param("/queue",queue, 5);
+            laser_input = nh_.subscribe(name_input, 10, &noiseFilter::Callback, this);
+            laser_output = nh_.advertise<sensor_msgs::LaserScan>(name_output, 10);
         }
 
 
@@ -18,21 +23,29 @@ class noiseFilter{
         ros::NodeHandle nh_;
         ros::Subscriber laser_input;
         ros::Publisher laser_output;
-         sensor_msgs::LaserScan laser_old;
-         sensor_msgs::LaserScan laser_msg;
-        bool isFirst;
+        std::queue<sensor_msgs::LaserScan> buffe;
+        //sensor_msgs::LaserScan laser_old;
+        sensor_msgs::LaserScan laser_msg;
+        bool isFull;
         std::string name_input;
         std::string name_output;
         double limit;
+        int count;
+        int queue;
+
 
         void Callback(const sensor_msgs::LaserScan& msg){
-            if(isFirst==true){
-                laser_old=msg;
+            if(isFull==false){
+                buffe.push(msg);
+                //laser_old=msg;
                 laser_output.publish(msg);
-                isFirst=false;
+                count++;
+                if(count==(queue-1))
+                    isFull=true;
                 return ;
             }
             else{
+                buffe.push(msg);
                 laser_msg.header=msg.header;
                 laser_msg.angle_min=msg.angle_min;
                 laser_msg.angle_max=msg.angle_max;
@@ -42,21 +55,36 @@ class noiseFilter{
                 laser_msg.range_min=msg.range_min;
                 laser_msg.range_max=msg.range_max;
                 laser_msg.ranges.resize(msg.ranges.size());
+                std::vector<sensor_msgs::LaserScan> temp;
+                    for(int i=0;i<queue;++i){
+                        temp.push_back(buffe.front());buffe.pop();
+                    }
+                    for(int i=1;i<queue;i++){
+                        buffe.push(temp[i]);
+                    }
                 for (int i=0;i<msg.ranges.size();++i){
-                    double percentage=(msg.ranges[i]-laser_old.ranges[i])/laser_old.ranges[i];
-                    ROS_INFO_STREAM("current error between two adjacent point is "<<percentage);
-                    nh_.param("limit",limit,0.2);
-                    if(percentage<-1*limit){
-                        laser_msg.ranges[i]=laser_old.ranges[i];
+                    //double percentage=(msg.ranges[i]-laser_old.ranges[i])/laser_old.ranges[i];
+                    //ROS_INFO_STREAM("current error between two adjacent point is "<<percentage);
+                    //nh_.param("/limit",limit,0.2);
+                    //if(percentage<-1*limit){
+                    //    laser_msg.ranges[i]=laser_old.ranges[i];
+                    //}
+                    //else{
+                    //    laser_msg.ranges[i]=msg.ranges[i];
+                    //}
+                    float max=0;
+                    BOOST_FOREACH(sensor_msgs::LaserScan& laser, temp)
+                    {
+                        if(laser.ranges[i]>max)
+                            max=laser.ranges[i];
                     }
-                    else{
-                        laser_msg.ranges[i]=msg.ranges[i];
-                    }
+                    laser_msg.ranges[i]=max;
                 }
                 laser_output.publish(laser_msg);
+               
             }
             
-            laser_old=msg;
+            //laser_old=msg;
             return ;
         }
 
